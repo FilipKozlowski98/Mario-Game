775cede2fda9fd0c49bfbc4543a61634b252393e
Filip Kozłowski <f.kozlowski98@gmail.com>
2021-11-26 04:12:15 +0100

    initial setup in progress

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Mario-Game
+
 Mario Game made with Kaboom.js
+
+Kabbom.js version : 0.5.0 (newer versions available)
+
+This game will not work on other Kaboom versions
--- game.js
+++ game.js
@@ -1,21 +1,20 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 1,
+  scale: 1.5,
   debug: true,
   clearColor: [0, 0, 0, 1],
 });
 
 loadRoot("./sprites/");
 loadSprite("coin", "coin.png");
-loadSprite("evilShroomLeftLeg", "evilShroomLeftLeg.png");
-loadSprite("evilShroomRightLeg", "evilShroomRightLeg.png");
+loadSprite("evilShroom", "evilShroom.png");
 loadSprite("brick", "brick.png");
 loadSprite("block", "block.png");
 loadSprite("mario", "mario.png");
 loadSprite("mushroom", "mushroom.png");
 loadSprite("surprise", "surprise.png");
-loadSprite("unboxed", "block.png");
+loadSprite("unboxed", "unboxed.png");
 loadSprite("pipeTopLeft", "pipeTopLeft.png");
 loadSprite("pipeTopRight", "pipeTopRight.png");
 loadSprite("pipeBottomLeft", "pipeBottomLeft.png");
@@ -29,20 +28,49 @@ scene("game", () => {
     "                                   ",
     "                                   ",
     "                                   ",
-    "                                   ",
-    "                                   ",
-    "                                   ",
-    "                                   ",
-    "=============================  ====",
+    "                      @            ",
+    "                    ====           ",
+    "                 @                 ",
+    "               ====                ",
+    "           @                       ",
+    "=*=       ==%=                     ",
+    "                                () ",
+    "                    <           {} ",
+    "==========================     ====",
   ];
 
   const levelCfg = {
     width: 20,
     height: 20,
-    "=": [sprite("block", solid())],
+    "=": [sprite("block"), solid()],
+    "@": [sprite("coin")],
+    "%": [sprite("surprise"), solid(), "coin-surprise"],
+    "*": [sprite("surprise"), solid(), "mushroom-surprise"],
+    "^": [sprite("unboxed"), solid()],
+    "(": [sprite("pipeTopLeft"), solid(), scale(0.5)],
+    ")": [sprite("pipeTopRight"), solid(), scale(0.5)],
+    "{": [sprite("pipeBottomLeft"), solid(), scale(0.5)],
+    "}": [sprite("pipeBottomRight"), solid(), scale(0.5)],
+    "<": [sprite("evilShroom"), solid()],
+    "#": [sprite("mushroom"), solid()],
   };
 
   const gameLevel = addLevel(map, levelCfg);
+
+  const scoreLabel = add([
+    text(score),
+    pos(30, 6),
+    layer("ui"),
+    { value: score },
+  ]);
+
+  const player = add([
+    sprite("mario"),
+    solid(),
+    pos(30, 0),
+    body(),
+    origin("bot"),
+  ]);
 });
 
 start("game");
